1ac0211f01b53102b9c0ea1dd0123e138cc0db1d
MoussioG4 <73383266+MoussioG4@users.noreply.github.com>
2022-08-04 15:32:41 +0300

    Solution to module 4 assignment

--- module4_assignment/SpeakGoodBye.js
+++ module4_assignment/SpeakGoodBye.js
@@ -8,15 +8,14 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
    var byeSpeaker = {};
-   byeSpeaker.speak = speak;
-
+  
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
   var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-  speak = function (name) {
+  byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
 
